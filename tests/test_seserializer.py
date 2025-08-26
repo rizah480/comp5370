d2bef1ee4d14e3ada7efca59e9fb7503086882f4
Air991 <Athomas@LucidTrades.com>
2025-08-26 12:59:16 -0400

    decode seems to work so far for a negative binary str

--- tests/test_seserializer.py
+++ tests/test_seserializer.py
@@ -1,3 +1,10 @@
+import Deserializer.deserializer as deserializer
+
+
+
+
 def test_add_two_numbers():
-    assert 1 + 1 == 1
+    val : str = '110'
+    assert deserializer.Deserializer.decode_num(val) == -2
+    
     
