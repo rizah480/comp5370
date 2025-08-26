7d8d88732260fadf299d97ac2f9bd24c5f46cff6
Air991 <Athomas@LucidTrades.com>
2025-08-26 13:51:50 -0400

    testing .decode_num function of deserializer

--- tests/test_seserializer.py
+++ tests/test_seserializer.py
@@ -3,8 +3,23 @@ import Deserializer.deserializer as deserializer
 
 
 
-def test_add_two_numbers():
+def test_add_two_numbers_negative_MSB():
     val : str = '110'
     assert deserializer.Deserializer.decode_num(val) == -2
+
+def test_add_two_numbers_positive():
+    val : str = '010'
+    assert deserializer.Deserializer.decode_num(val) == 2
+    
+def test_add_two_numbers_negative_2():
+    val : str = '11110110'
+    assert deserializer.Deserializer.decode_num(val) == -10
+    
+def test_add_two_numbers_positive_2():
+    val : str = '01110110'
+    assert deserializer.Deserializer.decode_num(val) == 118
+
+    
+    
     
     
