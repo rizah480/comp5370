d9dc3adf07c8525c7c5f5fda6f9ea175a0f30fc3
Air991 <Athomas@LucidTrades.com>
2025-08-26 15:53:23 -0400

    successfully created wrapper function to consume data and verify if it is safe to consume

--- Deserializer/deserializer.py
+++ Deserializer/deserializer.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Deserializer:
 
 
@@ -11,6 +14,15 @@ class Deserializer:
         if val & (1 << (n - 1)):    # check sign bit
             val -= (1 << n)         # subtract 2^n if negative
         return val
+    
+    
+    @staticmethod
+    def process_num(val: str) -> int:
+        if re.match(r"^[01]+$", val) is None:
+            raise ValueError("Input string must be a binary string")
+        
+        return Deserializer.decode_num(val)
+        
     
     
     
--- tests/test_seserializer.py
+++ tests/test_seserializer.py
@@ -18,6 +18,20 @@ def test_add_two_numbers_negative_2():
 def test_add_two_numbers_positive_2():
     val : str = '01110110'
     assert deserializer.Deserializer.decode_num(val) == 118
+    
+def test_process_num_invalid_input():
+    val : str = '01201'
+    try:
+        deserializer.Deserializer.process_num(val)
+        assert False, "Expected ValueError"
+    except ValueError as e:
+        assert str(e) == "Input string must be a binary string"
+        
+def test_process_num_valid_input():
+    val : str = '1010'
+    assert deserializer.Deserializer.process_num(val) == -6
+    
+    
 
     
     
